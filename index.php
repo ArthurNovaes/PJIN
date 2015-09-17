c2d2ea1f04756fcad32818418ecb76f74bb50cd4
ArthurNovaes <arthurnovaes9@gmail.com>
2015-09-17 20:56:17 -0300

    Update index.php

--- index.php
+++ index.php
@@ -8,25 +8,39 @@
 </head>
 <body>
 	<div class="container">
-		<h1>Busca de Orgãos</h1>
+		<h1>Estados onde estão ocorrendo obras do PAC</h1>
+
 		<div class="row">
 			<form action="dados.php" method="GET" class="form-inline">
-				<select class="form-control" name="orgao" id="">
-					<?php  
-						$arquivo=file_get_contents("orgaos.txt");
-						$separa=explode("\n", $arquivo); // divide o txt em linhas
-						$qtd_linhas=count($separa);
-						foreach ($separa as $a)
-						{
-							$separa1=explode(" ", $a);
-							$codigo=$separa1[0];
-
-							$orgao=substr($a,strlen($codigo));
-							//echo $codigo." - ".$orgao."<br>";
-							//echo $a."<br>";
-							echo "<option value='".$codigo."'>".$orgao."</option>";
-						}
-					?>
+				<select class="form-control" name="uf" id="uf">
+					
+					<option value="AC">Acre</option>
+					<option value="AL">Alagoas</option>
+					<option value="AP">Amapá</option>
+					<option value="AM">Amazonas</option>
+					<option value="BA">Bahia</option>
+					<option value="CE">Ceará</option>
+					<option value="DF">Distrito Federal</option>
+					<option value="ES">Espirito Santo</option>
+					<option value="GO">Goiás</option>
+					<option value="MA">Maranhão</option>
+					<option value="MS">Mato Grosso do Sul</option>
+					<option value="MT">Mato Grosso</option>
+					<option value="MG">Minas Gerais</option>
+					<option value="PA">Pará</option>
+					<option value="PB">Paraíba</option>
+					<option value="PR">Paraná</option>
+					<option value="PE">Pernambuco</option>
+					<option value="PI">Piauí</option>
+					<option value="RJ">Rio de Janeiro</option>
+					<option value="RN">Rio Grande do Norte</option>
+					<option value="RS">Rio Grande do Sul</option>
+					<option value="RO">Rondônia</option>
+					<option value="RR">Roraima</option>
+					<option value="SC">Santa Catarina</option>
+					<option value="SP">São Paulo</option>
+					<option value="SE">Sergipe</option>
+					<option value="TO">Tocantins</option>
 				</select>
 				<button type="submit" class="btn btn-primary"><i class="glyphicon glyphicon-search"></i> Buscar</button>
 			</form>
